9efa3e4d97c9edc317fcbd9c84da88ce75f12f23
SeungMin Lee <lsm9706@gmail.com>
2017-07-01 20:31:25 +0900

    [Gallery] (tmp) Access to original Gallery

--- app/src/main/java/com/example/q/project1/Tab2Gallery.java
+++ app/src/main/java/com/example/q/project1/Tab2Gallery.java
@@ -1,16 +1,48 @@
 package com.example.q.project1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Tab2Gallery extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2gallery, container, false);
+
+        Button btnNewImg = rootView.findViewById(R.id.btnNewImg);
+        btnNewImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 0);
+            }
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+//        Toast.makeText(getContext(), "ByeByeBye", Toast.LENGTH_LONG).show();
+        if(resultCode == RESULT_OK){
+            if(requestCode == 0){
+//                currImageURI = data.getData();
+                Toast.makeText(getContext(), data.getData().toString(), Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
